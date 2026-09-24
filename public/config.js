782f21b90f7d523ae7f4477a3da8e6ba31740321
RESALE-OG <resaleorigins@gmail.com>
2026-09-24 10:32:03 +0530

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,3 +1,3 @@
 window.SUPABASE_URL = "https://ujuafskszyojksdilnig.supabase.co";
 
-window.SUPABASE_ANON_KEY = "YAHAN_PUBLISHABLE_KEY_PASTE_KARO";
+window.SUPABASE_ANON_KEY = "sb_publishable__M9q7Vp_E7PSAVsxIQoWUQ_RhDOdXbT";
